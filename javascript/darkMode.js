ab3ef30ad155bced7613d98313a7de09037782a7
zzgh06 <zzgh06@naver.com>
2024-03-27 00:11:25 +0900

    feat : SearchModal 디자인 작성 중

--- javascript/darkMode.js
+++ javascript/darkMode.js
@@ -3,13 +3,13 @@ let input = document.querySelector('.input');
 input.addEventListener('click', () => {
   cnt++;
   // console.log(cnt)
-  if (cnt % 2 === 1) {
-    document.body.classList.add('dark-bg');
-    document.querySelector('.nav-bg').style.backgroundColor = '#333333';
-    document.querySelector('.dropbtn').style.cssText  = 'color: #FFFFFF; background-color: #333333';
-  } else {
-    document.body.classList.remove('dark-bg');
-    document.querySelector('.nav-bg').style.backgroundColor = '#FFFFFF';
-    document.querySelector('.dropbtn').style.cssText  = 'color: #333333; background-color: #FFFFFF';
-  }
+  document.body.classList.toggle('dark-bg');
+  // if (cnt % 2 === 1) {
+  //   document.querySelector('.nav-bg').style.backgroundColor = '#333333';
+  //   document.querySelector('.dropbtn').style.cssText  = 'color: #FFFFFF; background-color: #333333';
+  // } else {
+  //   document.body.classList.remove('dark-bg');
+  //   document.querySelector('.nav-bg').style.backgroundColor = '#FFFFFF';
+  //   document.querySelector('.dropbtn').style.cssText  = 'color: #333333; background-color: #FFFFFF';
+  // }
 });
