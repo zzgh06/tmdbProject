d2680fbb4d4eb4809adfbfe39366fb1fca8298cd
zzgh06 <zzgh06@naver.com>
2024-04-04 10:39:14 +0900

    Fix : 인피니티스크롤시 데이터가 1번이상 한꺼번에 불러오는 현상 수정

--- javascript/infiniteScroll.js
+++ javascript/infiniteScroll.js
@@ -1,27 +1,26 @@
 let alertDisplayed = false;
 let num = 1;
 
-window.addEventListener('scroll', function() {
-  // console.log(window.innerHeight)
-  // console.log(window.scrollY)
-  // console.log(document.body.offsetHeight)
-  // console.log(alertDisplayed)
+function 무한영화데이터() {
+  return fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-kr&page=${num + 1}&sort_by=popularity.desc`, options)
+    .then((response) => { 
+      return response.json();
+    })
+    .catch(err => console.error(err));
+}
+
+function handleScroll() {
   if (window.innerHeight + window.scrollY + 5 >= document.body.offsetHeight && !alertDisplayed) {
-    // alert('다읽음')
-    fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-kr&page=${num + 1}&sort_by=popularity.desc`, options)
-      .then(response => response.json())
+    alertDisplayed = true; // 한 번만 실행되도록 플래그 설정
+    무한영화데이터()
       .then(data => {
-        movies = data.results.slice(0, 16);
-        console.log('영화리스트 : ', movies);
-        무한생성(movies);
+        무한생성(data.results);
+        num++; // 페이지 번호 증가
+        alertDisplayed = false; // 플래그 재설정
       })
       .catch(err => console.error(err));
-    num += 1;
-    if (num === 1000) {
-      alertDisplayed = true;
-    }
   }
-});
+}
 
 function 무한생성(movies) {
   let infiniteMovies = movies.map(data =>
@@ -31,3 +30,5 @@ function 무한생성(movies) {
   );
   document.querySelector('.infinite-movie-list').insertAdjacentHTML('beforeend', infiniteMovies.join(''));
 }
+
+window.addEventListener('scroll', handleScroll);
